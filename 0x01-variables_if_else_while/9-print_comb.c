793df1d48db8132ac634bc9ca346e0cd356f4f64
Kingsley Bestor <bestorw@hotmail.com>
2022-04-10 21:03:05 +0100

    Added: 9-print_comb.c file

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+/*
+ *File: 9-print_comb.c
+ *Authour: Bestor Kingsley
+ */
+#include <stdio.h>
+/**
+ * main - prints numbers of base 10 from 0-9 with space and "," between.
+ *
+ * Return: Always 0 Success
+ */
+
+int main(void)
+{
+	char number;
+
+	for (number = '0'; number <= '9'; number++)
+	{
+		putchar(number);
+		putchar(',');
+		putchar(' ');
+	}
+	printf("\n");
+	return (0);
+}
+
