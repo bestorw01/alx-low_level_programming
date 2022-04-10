63889c939298ecd20023762659c7753762cd7f00
Kingsley Bestor <bestorw@hotmail.com>
2022-04-10 09:10:21 +0100

    Added: 5-print_numbers.c file

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+/*
+ *File: 5-print_numbers.c
+ *Authour: Bestor Kingsley
+ */
+#include <stdio.h>
+/**
+ * main - prints numbers of base 10 from 0-9.
+ *
+ * Return: Always 0 Success
+ */
+
+int main(void)
+{
+	char number;
+
+	for (number = '0'; number <= '9'; number++)
+	{
+		putchar(number);
+	}
+	printf("\n");
+	return (0);
+}
+
+
