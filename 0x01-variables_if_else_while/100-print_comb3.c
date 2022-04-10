081f9ecb9a106286f8a258d50778ebe6de2e6b00
Kingsley Bestor <bestorw@hotmail.com>
2022-04-10 21:55:14 +0100

    Added: 100-print_comb3.c file

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+/*
+* File:100-print_comb3.c
+* Authour: Bestor Kingsley
+*/
+#include <stdio.h>
+
+/**
+ * main - a simple program that outputs 0-9 separated by commas
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 48; i < 57; i++)
+	{
+		for (j = i + 1; j < 58; j++)
+		{
+			putchar(i);
+			putchar(j);
+			if (i != 56 || j != 57)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
