b20b4dfea4402626f740710d32fd82e5e2da141c
Kingsley Bestor <bestorw@hotmail.com>
2022-04-22 12:45:32 +0100

    Added: file

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+ * rev_string - reverses a string
+ * @s: holi
+ *
+ */
+void rev_string(char *s)
+{
+	int i, j, temp, length; 
+
+	while (s[i] != 0)
+	{
+		i++;
+	}
+
+	length = i;
+	i = 0;
+	j = length - 1;
+
+	while (i < j)
+	{
+		temp = s[i];
+		s[i] = s[j];
+		s[j] = temp;
+		i++;
+		j--;
+	}
+}
